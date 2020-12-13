9b4f81c059de312dfd1d56237762c0434ce4316c
Jahid Adnowar <milon5143@gmail.com>
2020-12-13 12:25:33 +0530

    State Change Bug Fixed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import store from './plugins/vuex'
+import store from './store/index'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
