9fc5b7cba32df9686ace0ca5ac4337919f768062
jahidanowar <milon5143@gmail.com>
2021-01-07 11:49:05 +0530

    d

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ export default new Vuex.Store({
   state: {
     token: localStorage.getItem("user-token") || null,
     user: {},
-    apiUrl:  process.env.API_URL || "http://binod.buzz/api/",
+    apiUrl:  process.env.API_URL || "https://binod.buzz/api/",
     cart: [],
     // {id, quantity}
     categories: null,
