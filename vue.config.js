ab6bb9f6bcddf58477d01e5e70408f97c4c4f0c2
jahidanowar <milon5143@gmail.com>
2021-01-08 19:43:48 +0530

    PWA Updated

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,6 @@ module.exports = {
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
-
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "dev/sw.js",
-      // ...other Workbox options...
-    },
     manifestOptions: {
       icons: [
         {
